c9b6558c3dd5b569c94d73a08619ca8022957995
Justin Tooley <jstooley823@gmail.com>
2017-09-04 22:45:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,19 +7,17 @@ end
 def batch_badge_creator (array_names)
   array_badge_message = []
   x = 0
-  array_names.each do |name|
-    array_badge_message[x] = badge_maker(name)
-    x += 1
+  array_names.each_with_index do |name, i|
+    array_badge_message[i] = badge_maker(name)
   end
   return array_badge_message #gets returned
   end
 
 def assign_rooms(array_names)
   room_assigned = []
-  x = 0
-  array_names.each do |name|
-    room_assigned[x] = "Hello, #{name} You'll be assigned to room #{x+1}!"
-    x+=1
+  
+  array_names.each_with_index do |name, i|
+    room_assigned[i] = "Hello, #{name} You'll be assigned to room #{i+1}!"
   end
   room_assigned #gets returned
 end
