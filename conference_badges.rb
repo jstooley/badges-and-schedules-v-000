0007ce6aab7304fb72a598ce1fd0b82e074b9889
Justin Tooley <jstooley823@gmail.com>
2017-09-04 22:18:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def batch_badge_creator (array_names)
+  array_message = []
+  x = 0
+  array_names.each do |name|
+    array_message[x] = "Hi, I am #{name}"
+    x +=1
+  end
+  array_messgae
+  end
+end
+def badge_maker(name)
+end
